e2b820ebbb634925dca91704051bafd73f4027d8
Sylvain CAU <ashdevfr@gmail.com>
2019-12-22 18:32:54 -0800

    Remove subfolder and copy files to the root folder

--- encounter+/packer.sh
+++ encounter+/packer.sh
@@ -2,7 +2,10 @@
 
 CWD=`pwd`
 BASE_DIR="$1"
-PACK_FILE="$BASE_DIR/pack.xml"
+
+TMP_DIR="$BASE_DIR/.tmp_pack"
+mkdir -p "$TMP_DIR"
+PACK_FILE="$TMP_DIR/pack.xml"
 
 function kebab_case () {
   echo $1 | sed -e "s/ /-/" | tr '[:upper:]' '[:lower:]'
@@ -12,7 +15,7 @@ function process_dir () {
   pushd "$1"
   echo "process_dir $1"
   local dir="$1"
-  local parent_base=$([ -n "$2" ] && echo -e "$2/" || echo "")
+  local parent_base=$([ -n "$2" ] && echo -e "${2}_" || echo "")
   local parent=$([ -n "$3" ] && echo -e " parent=\"$3\"" || echo "")
   local sort=1
   for file in "$dir"/*; do
@@ -20,23 +23,27 @@ function process_dir () {
     if [ -d "$file" ]; then
       local group_name=`basename "$file"`
       local group_slug=`kebab_case "$group_name"`
-      echo -e "\t<group id=\"$ITEM_UUID\" sort=\"$sort\"$parent>" >> "$PACK_FILE"
-    	echo -e "\t\t<name>$group_name</name>" >> "$PACK_FILE"
-    	echo -e "\t\t<slug>$group_slug</slug>" >> "$PACK_FILE"
-    	echo -e "\t</group>" >> "$PACK_FILE"
-      sort=`expr $sort + 1`
-      process_dir "$file" "$parent_base$group_name" "$ITEM_UUID"
+      if [ "$group_name" != ".tmp_pack" ]
+    	then
+        echo -e "\t<group id=\"$ITEM_UUID\" sort=\"$sort\"$parent>" >> "$PACK_FILE"
+        echo -e "\t\t<name>$group_name</name>" >> "$PACK_FILE"
+        echo -e "\t\t<slug>$group_slug</slug>" >> "$PACK_FILE"
+        echo -e "\t</group>" >> "$PACK_FILE"
+        sort=`expr $sort + 1`
+        process_dir "$file" "$parent_base$group_name" "$ITEM_UUID"
+      fi
     elif [ -f "$file" ]; then
       local resource_bname=`basename "$file"`
     	local resource_name="${resource_bname%.*}"
     	if [ "${file##*.}" != ".xml" ]
     	then
-    		echo -e "\t<asset id=\"${ITEM_UUID}\"$parent>" >> "$PACK_FILE"
-    		echo -e "\t\t<name>${resource_name}</name>" >> "$PACK_FILE"
-    		echo -e "\t\t<resource>$parent_base${resource_bname}</resource>" >> "$PACK_FILE"
-    		echo -e "\t\t<type>image</type>" >> "$PACK_FILE"
-    		echo -e "\t\t<size></size>" >> "$PACK_FILE"
-    		echo -e "\t</asset>" >> "$PACK_FILE"
+        echo -e "\t<asset id=\"${ITEM_UUID}\"$parent>" >> "$PACK_FILE"
+        echo -e "\t\t<name>${resource_name}</name>" >> "$PACK_FILE"
+        echo -e "\t\t<resource>$parent_base${resource_bname}</resource>" >> "$PACK_FILE"
+        echo -e "\t\t<type>image</type>" >> "$PACK_FILE"
+        echo -e "\t\t<size></size>" >> "$PACK_FILE"
+        echo -e "\t</asset>" >> "$PACK_FILE"
+        cp "$file" "$TMP_DIR/$parent_base${resource_bname}"
     	fi
     fi
   done
@@ -47,7 +54,6 @@ function process_dir () {
 PACK=`basename "$BASE_DIR"`
 SLUG=`kebab_case "$PACK"`
 UUID=`uuidgen`
-echo -e "Generating ${PACK}.pack"
 echo -e "Generating ${PACK}.pack"
 
 pushd "$1"
@@ -65,8 +71,10 @@ echo -e "\t<image></image>" >> "$PACK_FILE"
 process_dir "$BASE_DIR"
 
 echo -e "</pack>" >> "$PACK_FILE"
-# zip "../${PACK}.zip" *.*
-# pushd ..
-# mv "${PACK}.zip" "${PACK}.pack"
-# popd
+pushd "$TMP_DIR"
+ls -l
+zip -r "${PACK}.zip" * -x "${PACK}.zip"
+popd
+mv "$TMP_DIR/${PACK}.zip" "../${PACK}.pack"
+rm -rf "$TMP_DIR"
 popd
